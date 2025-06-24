2bf588de87fce8f4cf4b9208a1b5e954df40e1f3
1422756921 <1422756921@qq.com>
2025-06-24 15:18:53 +0800

    1

---
+++ yxmb/57/js/a.js
@@ -0,0 +1,29 @@
+﻿function a_init() {
+	updateShare(0);
+}
+
+function goHome() {
+	window.location.href ='http://www.qinglin.co';
+}
+
+function a_submitScore(score) {
+	updateShareScore(score);
+	setTimeout( function() { show_share(); }, 1500 )
+}
+
+function updateShare(bestScore) {
+	imgUrl = 'http://www.qinglin.co/img/logo.png';
+	lineLink = 'http://www.qinglin.co';
+	descContent = "这3D转啊转到我头晕！";
+	updateShareScore(bestScore);
+	appid = '';
+}
+
+function updateShareScore(bestScore) {
+	if(bestScore > 0) {
+		shareTitle = "我在《3D拼图》过了第" + bestScore + "关，这样子拼图好难啊！";
+	}
+	else{
+		shareTitle = "另类拼图游戏《3D拼图》好难啊！";
+	}
+}
---
+++ yxmb/82/index.md
@@ -0,0 +1,5 @@
+# Life Restart
+
+重新来过。然后，接下来会顺利进行。[RESTART](view/index.html)
+
+<iframe src="https://discord.com/widget?id=883382868427014255&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
